80543cb90408caa826fd8ea8b62dab914297f9b1
Nico <nicom514@hotmail.com>
2023-03-27 16:39:25 -0300

    primer versión 3ra preentrega: creación de card, evento click en cara rubro, envío a localstorage, recupero y muestra en navgador (solo puedo ver el último elemento del array recuperado)

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+const cards=document.querySelectorAll('.card')
+
+cards.forEach(card => {
+  card.addEventListener('click', function() {
+    const rubros = card.getAttribute('data-rubro');
+    console.log(rubros);
+    busquedaRubro=rubro.filter((item)=>item.rubro==rubros);
+    const stringRubros = JSON.stringify(busquedaRubro)
+    localStorage.setItem('rubros', stringRubros)
+    console.log(busquedaRubro)
+    console.log(typeof busquedaRubro)
+    mostrarRubro();
+  })
+});
+
+function mostrarRubro(){
+  
+  const resultadoRubro= localStorage.getItem('rubros')
+  const arrayRubro=JSON.parse(resultadoRubro)
+  console.log(arrayRubro)
+  console.log(typeof arrayRubro)
+  const muestraRubro = document.querySelector('#muestraRubro');
+  for (const negocio of arrayRubro){
+    muestraRubro.innerHTML=`
+    <div id="resultado"><p>${negocio.rubro} - ${negocio.negocio}</p></div>  `
+  }
+}
+
